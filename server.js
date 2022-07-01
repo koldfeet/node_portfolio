160f8bf0e688dca92ac802eb49aa7fa4c820084f
koldfeet <chris.tr88@gmail.com>
2022-07-01 16:38:43 -0600

    error handler added

--- server.js
+++ server.js
@@ -9,26 +9,26 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "/views"))
 app.use(express.static(path.join(__dirname, "public")))
 
-app.get("/", (request, response) => {
-    response.render("home.ejs")
-})
-
-app.get("/projects", (request, response) => {
+app.get("/projects", (request, response, next) => {
     response.render("projects.ejs")
 })
 
-app.get("/gallery", (request, response) => {
+app.get("/gallery", (request, response, next) => {
     response.render("gallery.ejs")
 })
 
-// ======== contact routes START "CRUD" =========
-app.get("/contacts", (request, response) => {
+app.get("/contacts", (request, response, next) => {
     response.render("contacts.ejs")
 })
 
+app.get("/", (request, response, next) => {
+    response.render("home.ejs")
+})
 
-
-// ======== contact routes END "CRUD" =========
+// ======= handle error for when page not found =========
+app.use((request, response, next) => {
+    response.status(404).send("<h1> Page not found </h1>")
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listenning on PORT: 5000`)
