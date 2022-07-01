93752dd3e7b5e8610734eccaa67e79d4dc09185e
koldfeet <chris.tr88@gmail.com>
2022-06-30 20:20:19 -0600

    contact back to start

--- public/app.js
+++ public/app.js
@@ -48,42 +48,6 @@ for (let i = 0; i < cards.length; i++) {
 // ======= project section cards end ========
 
 // ======= Contact page START ==========
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("http://localhost:5000/getAll")
-        .then(response => response.json())
-        .then(data => loadHTMLTable(data["data"]))
-})
 
-const addBtn = document.querySelector("#add-name-btn")
-
-addBtn.onclick = function () {
-    const nameInput = document.querySelector("#name-input")
-    const name = nameInput.value;
-    nameInput.value = "";
-
-    fetch("http://localhost:5000/insert", {
-        headers: {
-            "Content-type": "application/json"
-        },
-        method: "POST",
-        body: JSON.stringify({ name: name })
-    })
-        .then(response => response.json())
-        .then(data => insertRowIntoTable(data["data"]))
-}
-
-function insertRowIntoTable(data) {
-
-}
-
-function loadHTMLTable(data) {
-    const table = document.querySelector("table tbody")
-
-    console.log(data)
-
-    if (data.length === 0) {
-        table.innerHTML = "<tr><td class='no-data' colspan = '7'> No Data </td></tr>"
-    }
-}
 
 // ======= Contact page end ==========
--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 const express = require("express")
 const app = express()
-const cors = require("cors") //make api calls from frontend to backend
 const dotenv = require("dotenv").config()
 const PORT = process.env.PORT
 const path = require("path")
 const mysql = require("mysql")
-const dbService = require("./dbService.js") //import dbService page
-// const { response } = require("express")
-
-
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "/views"))
@@ -34,21 +26,6 @@ app.get("/contacts", (request, response) => {
     response.render("contacts.ejs")
 })
 
-//create
-app.post("/insert", (request, response) => {
-    console.log(request.body)
-})
-
-//read
-app.get("/getAll", (request, response) => {
-    const db = dbService.getDbServiceInstance()
-
-    const result = db.getAllData()
-
-    result
-        .then(data => response.json({ data: data }))
-        .catch(err => console.log(err))
-})
 
 
 // ======== contact routes END "CRUD" =========
